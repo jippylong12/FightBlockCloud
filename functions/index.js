c5d2b29c1d1ee444276d80a8fb8be84dcc99e8cf
marcus.salinas <12.marcus.salinas@gmail.com>
2021-12-26 21:37:28 +0400

    After much testing I got a function working where we can query the API and pull the events and add them to the DB for a select year. This will be able to run every hour and will not add duplicates

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,46 @@
 const functions = require("firebase-functions");
+const fdClientModule = require('fantasydata-node-client');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const keys = {
+    'MMAv3StatsClient': 'f1914d5079c141b9bf2fd101292e8f3c',
+    'MMAv3ScoresClient': 'f1914d5079c141b9bf2fd101292e8f3c'
+};
+
+
+exports.getMMASchedule = functions.pubsub.schedule('every 1 hour').onRun((context) => {
+    console.log('This will be run every 5 minutes!');
+    return null;
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+
+    const FantasyDataClient = new fdClientModule(keys);
+    let writeResult = {id: 0}
+    let snapshot = await admin.firestore().collection("events").get().then(querySnapshot => {
+        return querySnapshot.docs.map(doc => doc.data())
+    });
+
+    await FantasyDataClient.MMAv3ScoresClient.getSchedulePromise("UFC", 2021).then(async results => {
+      results = JSON.parse(results);
+
+      results.forEach(event => {
+
+          if(!snapshot.some(item => item.EventId === event['EventId'])){
+              functions.logger.info(`Adding event ${JSON.stringify(event)}`, {structuredData: true});
+              writeResult = admin.firestore().collection('events').add(event);
+          }
+      })
+
+    }).catch(error => {
+      functions.logger.error("Client failed!", {structuredData: true});
+      functions.logger.error(error, {structuredData: true});
+    })
+
+    response.send(`Processing`);
+});
