35d591ebb0d97226119e5d8e65f36c21a80ad2ce
marcus.salinas <12.marcus.salinas@gmail.com>
2022-02-01 15:41:33 +0400

    Saving spot I was at with updating scores

--- functions/index.js
+++ functions/index.js
@@ -127,251 +127,6 @@ exports.getMMAEventDetails = functions.pubsub.schedule('every 24 hours').onRun(a
 });
 
 exports.updateScores = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
-    function correctChosenFighter(pick) {
-        return pick['fighterIdChosen'] === pick['fightData']['WinnerId'];
-    }
-
-    function correctChosenMethod(pick) {
-        return pick['methodChosen'] === pick['fightData']['ResultType'];
-    }
-
-    function correctChosenRound(pick) {
-        return pick['roundChosen'] === pick['fightData']['ResultRound'];
-    }
-
-    function scorePick(pick){
-        let pickedWinner = false;
-
-        // only if the contest is done
-        if(pick['fightData']['Status'] === 'Final'){
-            pick['score'] = 0; // reset
-            pick['perfectHit'] = false;
-            // winner correct?
-            if (correctChosenFighter(pick)) {
-                pickedWinner = true;
-                pick['score'] += 2;
-                // method correct?
-                if (correctChosenMethod(pick)) {
-                    if (pick['methodChosen'] === 'DEC') {
-                        pick['score'] += 10;
-                    } else {
-                        // round correct?
-                        if (correctChosenRound(pick)) {
-                            pick['score'] += 10; // for method and round
-                            pick['perfectHit'] = true; // set true
-                            pick['score'] *= 3; // Perfect Hit multiplier
-                        } else {
-                            // just the points for the correct method
-                            pick['score'] += 4;
-                        }
-                    }
-                } else if(correctChosenRound(pick)){
-                    pick['score'] += 6;
-                }
-            } else {
-                // we do nothing since they didn't make the right pick
-            }
-
-            if (pick['FotNBool']) {
-                // double score of the bout
-                pick['score'] *= 2;
-            }
-        }
-
-        return pickedWinner;
-    }
-
-    // loop through the pickList and score it
-    function scorePickList(pickList) {
-        pickList['score'] = 0;
-        let boutWinnerCount = 0;
-        for (let thisPick in pickList['picks']) {
-            if(scorePick(thisPick)){
-                boutWinnerCount += 1;
-            }
-
-            pickList['score'] += thisPick['score'];
-        }
-
-        // if we pick all winners then we give the Parlay bonus
-        if (boutWinnerCount === pickList['picks'].length) {
-            for (let thisPick in pickList['picks']) {
-                thisPick['score'] += 6;
-                pickList['score'] += thisPick['score'];
-            }
-        }
-
-    }
-
-
-    let startDate = new Date();
-    let endDate = new Date();
-    endDate.setDate(endDate.getDate()+2);
-    startDate.setDate(startDate.getDate()-2);
-    let isoStringStart = startDate.toISOString();
-    let isoStringEnd = endDate.toISOString();
-
-    const FantasyDataClient = new fdClientModule(keys);
-    let snapshot = await admin.firestore().collection("events")
-        .where("DateTime",">", isoStringStart)
-        .where("DateTime","<", isoStringEnd)
-        .orderBy("DateTime", "desc").get().then(querySnapshot => {
-            return querySnapshot.docs.map(doc => doc.data())
-        });
-
-
-
-    console.log(snapshot);
-    // which leagues to update.
-    // {leagueId: {userId: points}}
-    let leagueUpdateMap = {}
-
-    snapshot.forEach(event => {
-        FantasyDataClient.MMAv3ScoresClient.getEventPromise(event['EventId']).then(async results => {
-            results = JSON.parse(results);
-
-            // find all pickLists with this event
-            await admin.firestore().collection("pickLists")
-                .where("EventId","==", event['EventId']).then(pickListsSnapshot => {
-                    pickListsSnapshot.docs.forEach(doc => {
-                        let pickList = doc.data();
-
-                        console.log(pickList);
-                        // replace the fight data with the right data
-                        pickList['picks'].forEach(pick => {
-                            results['Fights'].forEach((fight) => {
-                                if(pick['FightData']['FightId'] === fight['FightId']){
-                                    pick['FightData'] = fight;
-                                }
-                            });
-                        });
-
-                        // score the pickList
-                        // scorePickList(pickList);
-                        //
-                        // // update
-                        // admin.firestore().collection("pickLists").doc(doc.id).set(pickList);
-                        //
-                        // leagueUpdateMap[pickList['leagueId']][pickList['userId']] = pickList['score'];
-
-
-                    })
-
-                });
-
-
-
-            // update the league leaderboard
-            // for( let leagueId in leagueUpdateMap){
-            //     admin.firestore().collection("leagues").doc(leagueId).get().then(docSnapshot => {
-            //         let leagueData = docSnapshot.data();
-            //
-            //         leagueData['leaderboard'].forEach((userRow) => {
-            //             // if we have this user updated pickList then we replace it
-            //             if(leagueUpdateMap[leagueId].hasOwnProperty(userRow['userId'])){
-            //                 userRow['score'] = leagueUpdateMap[leagueId][userRow['userId']];
-            //             }
-            //         })
-            //
-            //         // update
-            //         admin.firestore().collection("leagues").doc(leagueId).set(leagueData);
-            //     });
-            // }
-
-
-
-        }).catch(error => {
-            functions.logger.error("Client failed!", {structuredData: true});
-            functions.logger.error(error, {structuredData: true});
-        })
-    });
-
-    return null;
-});
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
-
-    function correctChosenFighter(pick) {
-        return pick['fighterIdChosen'] === pick['fightData']['WinnerId'];
-    }
-
-    function correctChosenMethod(pick) {
-        return pick['methodChosen'] === pick['fightData']['ResultType'];
-    }
-
-    function correctChosenRound(pick) {
-        return pick['roundChosen'] === pick['fightData']['ResultRound'];
-    }
-
-    function scorePick(pick){
-        let pickedWinner = false;
-
-        // only if the contest is done
-        if(pick['fightData']['Status'] === 'Final'){
-            pick['score'] = 0; // reset
-            pick['perfectHit'] = false;
-            // winner correct?
-            if (correctChosenFighter(pick)) {
-                pickedWinner = true;
-                pick['score'] += 2;
-                // method correct?
-                if (correctChosenMethod(pick)) {
-                    if (pick['methodChosen'] === 'DEC') {
-                        pick['score'] += 10;
-                    } else {
-                        // round correct?
-                        if (correctChosenRound(pick)) {
-                            pick['score'] += 10; // for method and round
-                            pick['perfectHit'] = true; // set true
-                            pick['score'] *= 3; // Perfect Hit multiplier
-                        } else {
-                            // just the points for the correct method
-                            pick['score'] += 4;
-                        }
-                    }
-                } else if(correctChosenRound(pick)){
-                    pick['score'] += 6;
-                }
-            } else {
-                // we do nothing since they didn't make the right pick
-            }
-
-            if (pick['FotNBool']) {
-                // double score of the bout
-                pick['score'] *= 2;
-            }
-        }
-
-        return pickedWinner;
-    }
-
-    function showRankText(rank){}
-
-    // loop through the pickList and score it
-    function scorePickList(pickList) {
-        pickList['score'] = 0;
-        let boutWinnerCount = 0;
-
-        pickList['picks'].forEach((thisPick, index) => {
-            if(scorePick(thisPick)){
-                boutWinnerCount += 1;
-            }
-
-            pickList['score'] += thisPick['score'];
-        })
-
-        // if we pick all winners then we give the Parlay bonus
-        if (boutWinnerCount === pickList['picks'].length) {
-            for (let thisPick in pickList['picks']) {
-                thisPick['score'] += 6;
-                pickList['score'] += thisPick['score'];
-            }
-        }
-
-    }
-
 
     let working = true;
     let startDate = new Date();
@@ -483,9 +238,91 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
         })
     }
 
-    while(working){
+    while(working){}
+
+    return null;
+
+
+    function correctChosenFighter(pick) {
+        return pick['fighterIdChosen'] === pick['fightData']['WinnerId'];
+    }
+
+    function correctChosenMethod(pick) {
+        return pick['methodChosen'] === pick['fightData']['ResultType'];
+    }
+
+    function correctChosenRound(pick) {
+        return pick['roundChosen'] === pick['fightData']['ResultRound'];
+    }
+
+    function scorePick(pick){
+        let pickedWinner = false;
+
+        // only if the contest is done
+        if(pick['fightData']['Status'] === 'Final'){
+            pick['score'] = 0; // reset
+            pick['perfectHit'] = false;
+            // winner correct?
+            if (correctChosenFighter(pick)) {
+                pickedWinner = true;
+                pick['score'] += 2;
+                // method correct?
+                if (correctChosenMethod(pick)) {
+                    if (pick['methodChosen'] === 'DEC') {
+                        pick['score'] += 10;
+                    } else {
+                        // round correct?
+                        if (correctChosenRound(pick)) {
+                            pick['score'] += 10; // for method and round
+                            pick['perfectHit'] = true; // set true
+                            pick['score'] *= 3; // Perfect Hit multiplier
+                        } else {
+                            // just the points for the correct method
+                            pick['score'] += 4;
+                        }
+                    }
+                } else if(correctChosenRound(pick)){
+                    pick['score'] += 6;
+                }
+            } else {
+                // we do nothing since they didn't make the right pick
+            }
+
+            if (pick['FotNBool']) {
+                // double score of the bout
+                pick['score'] *= 2;
+            }
+        }
+
+        return pickedWinner;
+    }
+
+    // loop through the pickList and score it
+    function scorePickList(pickList) {
+        pickList['score'] = 0;
+        let boutWinnerCount = 0;
+        for (let thisPick in pickList['picks']) {
+            if(scorePick(thisPick)){
+                boutWinnerCount += 1;
+            }
+
+            pickList['score'] += thisPick['score'];
+        }
+
+        // if we pick all winners then we give the Parlay bonus
+        if (boutWinnerCount === pickList['picks'].length) {
+            for (let thisPick in pickList['picks']) {
+                thisPick['score'] += 6;
+                pickList['score'] += thisPick['score'];
+            }
+        }
 
     }
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
 
     response.send(`Processing`);
 });
