4c315e367e4e8280c0cab56d019a13b75c3fe130
marcus.salinas <12.marcus.salinas@gmail.com>
2022-05-19 02:20:36 +0400

    Updated getMMA fighters to update the fightersJSON if found

--- README.md
+++ README.md
@@ -34,10 +34,10 @@ https://console.cloud.google.com/firestore/import-export?project=fight-block
 You can find the folder name under the Bucket column
 ```shell
 gsutil -m cp -r \
-  "gs://development-exports/2022-05-07T14:14:05_64968" \
+  "gs://development-exports/2022-05-18T14:14:07_46600" \
   .
 rm -rf ~/fbExport && 
 mkdir ~/fbExport && 
-mv 2022-05-07T14:14:05_64968/* ~/fbExport/ && 
-rm -rf 2022-05-07T14:14:05_64968
+mv 2022-05-18T14:14:07_46600/* ~/fbExport/ && 
+rm -rf 2022-05-18T14:14:07_46600
 ```
--- functions/pubsubs/getMMAFighters.js
+++ functions/pubsubs/getMMAFighters.js
@@ -9,7 +9,7 @@ module.exports = async (context) => {
 
     const FantasyDataClient = new fdClientModule(Constants.keys);
     let snapshot = await admin.firestore().collection("fighters").get().then(querySnapshot => {
-        return querySnapshot.docs.map(doc => doc.data())
+        return querySnapshot.docs.map(doc => [doc.id, doc.data()])
     });
 
 
@@ -26,7 +26,8 @@ module.exports = async (context) => {
         console.log(results.length);
 
         results.forEach(fighter => {
-            if (snapshot.length === 0 || !snapshot.some(item => item.FighterId === fighter['FighterId'])) {
+            let foundFighter = snapshot.find(item => item[1].FighterId === fighter['FighterId']);
+            if (snapshot.length === 0 || foundFighter === undefined) {
                 if(counter <= 498){
                     batches[commitCounter].set(admin.firestore().collection('fighters').doc(), fighter)
                     counter = counter + 1;
@@ -34,6 +35,17 @@ module.exports = async (context) => {
                     counter = 0;
                     commitCounter = commitCounter + 1;
                     batches[commitCounter] = admin.firestore().batch();
+                    batches[commitCounter].set(admin.firestore().collection('fighters').doc(), fighter)
+                }
+            } else {
+                if(counter <= 498){
+                    batches[commitCounter].update(admin.firestore().collection('fighters').doc(foundFighter[0]), fighter)
+                    counter = counter + 1;
+                } else {
+                    counter = 0;
+                    commitCounter = commitCounter + 1;
+                    batches[commitCounter] = admin.firestore().batch();
+                    batches[commitCounter].update(admin.firestore().collection('fighters').doc(foundFighter[0]), fighter)
                 }
             }
         })
