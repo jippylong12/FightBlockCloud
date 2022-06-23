c8352882ab865663dbc4d12239c5adb23dd9ab11
marcus.salinas <12.marcus.salinas@gmail.com>
2022-06-23 22:57:15 +0400

    Updated getMMAEventDetails.js so that we can handle a better implementation of updating the fight data. The old way would delete the users' choices if the fights were remained by changed order like from 2-5 and 5-2. Both 5 and 2 would be deleted because the order changed.
    Made it a little robust so that the item that has to not be present on the card to be removed.

--- README.md
+++ README.md
@@ -34,10 +34,10 @@ https://console.cloud.google.com/firestore/import-export?project=fight-block
 You can find the folder name under the Bucket column
 ```shell
 gsutil -m cp -r \
-  "gs://development-exports/2022-06-05T07:06:40_54408" \
+  "gs://development-exports/2022-06-21T14:14:05_69032" \
   .
 rm -rf ~/fbExport && 
 mkdir ~/fbExport && 
-mv 2022-06-05T07:06:40_54408/* ~/fbExport/ && 
-rm -rf 2022-06-05T07:06:40_54408
+mv 2022-06-21T14:14:05_69032/* ~/fbExport/ && 
+rm -rf 2022-06-21T14:14:05_69032
 ```
--- functions/pubsubs/getMMAEventDetails.js
+++ functions/pubsubs/getMMAEventDetails.js
@@ -95,25 +95,26 @@ module.exports = async (context) => {
     // this will try to find the fight data in the pickList. If we find it then replace the data, if there is a clash of order
     // then we need to remove the old one because we assume it's been replaced
     // otherwise we just add it because we will be sorting it later
-    function updateFightData(fight, listData) {
+    function updateFightData(fight, listData, recentFightIds, allFightIds) {
         // exclude early prelims
         if(!(fight['CardSegment'] === 'Early Prelims')){
             // if the pick list doesn't allow prelims we don't want them either
             // always allow main card events
             if((fight['CardSegment'] === 'Prelims' && listData['prelims']) || fight['CardSegment'] === 'Main Card'){
+                allFightIds[fight['FightId']] = null;
 
                 let pickIndex = listData['picks'].findIndex(item => item['fightData']['FightId'] === fight['FightId']);
 
-                // we don't want these ones
 
                 if(pickIndex !== -1){
-                    // replace the current fight data
+                    // replace the current fight data if we find the same fightID
                     listData['picks'][pickIndex]['fightData'] = fight;
                 } else {
+                    // the fight no longer exists
                     // if there is an item in the list that has the same order we need to remove it
                     let foundIndex = listData['picks'].findIndex(item => item['fightData']['Order'] === fight['Order']);
                     if(foundIndex !== -1){
-                        listData['picks'].splice(foundIndex,1)
+                        recentFightIds[fight['Order']] = fight['FightId']
                     }
                     // then add the new item
                     listData['picks'].push({
@@ -129,6 +130,7 @@ module.exports = async (context) => {
                 }
             }
         }
+
     }
 
     // take a pick list and update the event data and then update the fighter data and finally sort and add to batch
@@ -146,54 +148,28 @@ module.exports = async (context) => {
             leagueUpdateMap[listData['leagueId']] = {[results['EventId']]: results};
         }
         // for each pick list, let's update the fight data
+        let recentFightIds = {}; // order => fightId We need to keep track of the most recent fight IDs so we know which to remove
+        let allFightIds = {}; // we will go through the array one more time to make sure that we keep only those available
         results['Fights'].forEach(function(fight) {
-            updateFightData(fight, listData);
+            updateFightData(fight, listData, recentFightIds);
         })
 
-        let indexesToRemove = [];
-        // we need to check that all the current fight data are still
-        listData['picks'].forEach(function (list) {
-
-            // get result
-            let resultIndex = results['Fights'].findIndex(item => list['fightData']['FightId'] === item['FightId']);
-            const fighterResult = results['Fights'][resultIndex];
-
-            if(resultIndex === -1 ) {
-                // fight doesn't exist so we need to remove this
-                let pickIndex = listData['picks'].findIndex(item => item['fightData']['FightId'] === list['fightData']['FightId']);
-                indexesToRemove.push(pickIndex)
-            } else if (fighterResult['CardSegment'] !== 'Main Card' && fighterResult['CardSegment'] !== 'Prelims') {
-                let pickIndex = listData['picks'].findIndex(item => item['fightData']['FightId'] === list['fightData']['FightId']);
-                indexesToRemove.push(pickIndex)
+        // add new fights, update any old fight data
+        listData['picks'] = listData['picks'].filter(function(pick) {
+            const order = pick['fightData']['Order']
+            if(recentFightIds.hasOwnProperty(order)) {
+                // if we don't match then we need to remove it otherwise we will retunr it because it's the latest one
+                return recentFightIds[order] === pick['fightData']['FightId'];
             } else {
-                // check order and CardSegment
-                let orderBool = fighterResult['Order'] === list['fightData']['Order'];
-                let cardSegmentBool = fighterResult['CardSegment'] === list['fightData']['CardSegment'];
-
-                // if the same do nothing otherwise check if this list has prelims or not and remove or keep based on value
-                if (orderBool && cardSegmentBool) {}
-                else {
-                    // if segment in results does not equal Prelims or Main Card we remove
-                    if (fighterResult['CardSegment'] !== 'Main Card' && fighterResult['CardSegment'] !== 'Prelims') {
-                        let pickIndex = listData['picks'].findIndex(item => item['fightData']['FightId'] === list['fightData']['FightId']);
-                        indexesToRemove.push(pickIndex)
-                    } else {
-                        if ( !list['prelims'] && fighterResult['CardSegment'] === 'Prelims') {
-                            let pickIndex = listData['picks'].findIndex(item => item['fightData']['FightId'] === list['fightData']['FightId']);
-                            indexesToRemove.push(pickIndex)
-                        }
-                    }
-                }
+                return true;
             }
-
-
         });
 
-        indexesToRemove.sort().reverse();
-
-        for (const index of indexesToRemove) {
-            listData['picks'].splice(index,1)
-        }
+        // filter out any fight ids we haven't seen.
+        listData['picks'] = listData['picks'].filter(function(pick) {
+            const fightId = pick['fightData']['FightId'];
+            return allFightIds.hasOwnProperty(fightId);
+        });
 
         listData['picks'].sort(sharedFunctions.sortByOrderPicks);
 
