fed438702d1237f530fb604a25663c1c5ac9b79d
marcus.salinas <12.marcus.salinas@gmail.com>
2023-01-11 01:04:58 +0400

    I think we got it

--- functions/requests/one-time/createLeagueScoresAndPositionsPerEvent.js
+++ functions/requests/one-time/createLeagueScoresAndPositionsPerEvent.js
@@ -7,6 +7,8 @@
 
 const FantasyAnalyticsClient = require("../../fa_api/fa_client");
 const admin = require("firebase-admin");
+const SharedFunctions = require("../../SharedFunctions");
+const sharedFunctions = new SharedFunctions();
 module.exports = async (request, response) => {
     let client = new FantasyAnalyticsClient();
     await client.login();
@@ -14,6 +16,12 @@ module.exports = async (request, response) => {
     let leagues = await admin.firestore().collection("leagues").get().then(querySnapshot => {
         return querySnapshot.docs.map(doc => doc)
     });
+
+    let counter = 0;
+    let commitCounter = 0;
+    let batches = [];
+
+    batches[commitCounter] = admin.firestore().batch();
 
 
     for (const league of leagues ) {
@@ -33,66 +41,81 @@ module.exports = async (request, response) => {
             positionPerWeek['default'][userId] = 1;
         }
 
-        for (const event of leagueData['events']) {
-            let pickLists = await admin.firestore().collection("pickLists")
-                .where("eventId", "==", event['EventId'])
-                .where("leagueId", "==", leagueId).get();
-            pickLists = pickLists.docs;
-            let thisWeekScoreMap = {} // userId -> score
-            let thisWeekPositionMap = {} // userId -> position
-            for (const pickList of pickLists) {
-                const pickListData = pickList.data();
-                const userId = pickListData['userId'];
-                thisWeekScoreMap[userId] = pickListData['score'];
-                if(!totalWeekScoreMap.hasOwnProperty(userId)) totalWeekScoreMap[userId] = 0;
-                totalWeekScoreMap[userId] += thisWeekScoreMap[userId];
-            }
-
-
-            // add the value of the users that didn't set picks this week
-            for (const userId of leagueData['memberIds']) {
-                if(!thisWeekScoreMap.hasOwnProperty(userId)) {
+        if(leagueData.hasOwnProperty('events')) {
+            for (const event of leagueData['events']) {
+                let pickLists = await admin.firestore().collection("pickLists")
+                    .where("eventId", "==", event['EventId'])
+                    .where("leagueId", "==", leagueId).get();
+                pickLists = pickLists.docs;
+                let thisWeekScoreMap = {} // userId -> score
+                let thisWeekPositionMap = {} // userId -> position
+                for (const pickList of pickLists) {
+                    const pickListData = pickList.data();
+                    const userId = pickListData['userId'];
+                    thisWeekScoreMap[userId] = pickListData['score'];
                     if(!totalWeekScoreMap.hasOwnProperty(userId)) totalWeekScoreMap[userId] = 0;
-                    thisWeekScoreMap[userId] = 0;
+                    totalWeekScoreMap[userId] += thisWeekScoreMap[userId];
                 }
+
+
+                // add the value of the users that didn't set picks this week
+                for (const userId of leagueData['memberIds']) {
+                    if(!thisWeekScoreMap.hasOwnProperty(userId)) {
+                        if(!totalWeekScoreMap.hasOwnProperty(userId)) totalWeekScoreMap[userId] = 0;
+                        thisWeekScoreMap[userId] = 0;
+                    }
+                }
+
+
+
+                // sort by value
+                const sortable = Object.fromEntries(
+                    Object.entries(totalWeekScoreMap).sort(([,a],[,b]) => b-a)
+                );
+
+
+                // create the position that week
+                let lastValue = -1; // if the last rank value is the same then we will use the previousRank
+                let previousRank = 1;
+                Object.entries(sortable).forEach(function(e, index) {
+                    let possibleRank = index + 1;
+                    const userId = e[0];
+                    const thisScore = e[1];
+                    if(lastValue === thisScore) {
+                        possibleRank = previousRank;
+                    } else {
+                        previousRank = possibleRank;
+                    }
+                    lastValue = thisScore;
+                    thisWeekPositionMap[userId] = possibleRank; // id -> userId -> position
+                })
+
+                // add to main objects
+                scoresPerWeek[event['Day']] = thisWeekScoreMap;
+                positionPerWeek[event['Day']] = thisWeekPositionMap;
             }
-
-
-
-            // sort by value
-            const sortable = Object.fromEntries(
-                Object.entries(totalWeekScoreMap).sort(([,a],[,b]) => b-a)
-            );
-
-
-            // create the position that week
-            let lastValue = -1; // if the last rank value is the same then we will use the previousRank
-            let previousRank = 1;
-            Object.entries(sortable).forEach(function(e, index) {
-                let possibleRank = index + 1;
-                const userId = e[0];
-                const thisScore = e[1];
-                if(lastValue === thisScore) {
-                    possibleRank = previousRank;
-                } else {
-                    previousRank = possibleRank;
-                }
-                lastValue = thisScore;
-                thisWeekPositionMap[userId] = possibleRank; // id -> userId -> position
-            })
-
-            // add to main objects
-            scoresPerWeek[event['Day']] = thisWeekScoreMap;
-            positionPerWeek[event['Day']] = thisWeekPositionMap;
         }
+
 
         leagueData['scoresPerWeek'] = scoresPerWeek;
         leagueData['positionPerWeek'] = positionPerWeek;
 
-        // await admin.firestore().collection("leagues").doc(leagueId).set(leagueData);
 
-
+        // gather to update the league ids by batch
+        if(counter <= 498){
+            batches[commitCounter].set(admin.firestore().collection('leagues').doc(leagueId), leagueData)
+            counter = counter + 1;
+        } else {
+            counter = 0;
+            commitCounter = commitCounter + 1;
+            batches[commitCounter] = admin.firestore().batch();
+            batches[commitCounter].set(admin.firestore().collection('leagues').doc(leagueId), leagueData)
+        }
     }
+
+
+    await sharedFunctions.writeToDb(batches);
+    response.send(`Processed`);
 
 
 }
